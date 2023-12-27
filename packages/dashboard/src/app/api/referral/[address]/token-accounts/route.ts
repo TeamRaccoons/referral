3d733ad9c6a57ed183de41c4457c130998bf4806
zhengyutay <tayzhengyu@gmail.com>
2023-12-27 16:22:34 +0800

    fix: disable token-accounts route

--- packages/dashboard/src/app/api/referral/[address]/token-accounts/route.ts
+++ packages/dashboard/src/app/api/referral/[address]/token-accounts/route.ts
@@ -1,28 +1,17 @@
 import { NextResponse } from "next/server";
-import { ReferralProvider } from "@jup-ag/referral-sdk";
-import { Connection } from "@solana/web3.js";
-
-import { RPC_URL } from "@/lib/constants";
 
 export const runtime = "edge";
 
-export async function GET(
-  request: Request,
-  { params: { address } }: { params: { address: string } },
-) {
-  const connection = new Connection(RPC_URL);
-  const referralProvider = new ReferralProvider(connection);
-
-  const tokenAccounts = await referralProvider.getReferralTokenAccounts(
-    address,
-  );
-
+export async function GET() {
+// request: Request,
+// { params: { address } }: { params: { address: string } },
   return NextResponse.json(
-    [...tokenAccounts.token2022Accounts, ...tokenAccounts.tokenAccounts].map(
-      ({ pubkey, account }) => ({
-        pubkey: pubkey.toString(),
-        mint: account.mint.toString(),
-      }),
-    ),
+    {
+      error:
+        "This endpoint is disabled. Please contact us on discord for assistance.",
+    },
+    {
+      status: 403,
+    },
   );
 }
