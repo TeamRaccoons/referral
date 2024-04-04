7bae2334fbd5ca46548d7b786858dd69d93d3499
Sin Wai Kit <31162981+wkitsin@users.noreply.github.com>
2024-04-04 19:11:32 +0800

    revert: claim all route

--- packages/dashboard/src/app/api/referral/[address]/claim-all/route.ts
+++ packages/dashboard/src/app/api/referral/[address]/claim-all/route.ts
@@ -1,15 +1,46 @@
 import { NextResponse } from "next/server";
+import { ReferralProvider } from "@jup-ag/referral-sdk";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { z } from "zod";
+
+import { RPC_URL } from "@/lib/constants";
 
 export const runtime = "edge";
 
-export async function POST() {
-  return NextResponse.json(
-    {
-      error:
-        "This endpoint is disabled. Please contact us on discord for assistance.",
-    },
-    {
-      status: 403,
-    },
-  );
+const bodyShape = z.object({
+  referralAccount: z.string(),
+  feePayer: z.string(),
+});
+
+export async function POST(
+  request: Request,
+  { params: { address } }: { params: { address: string } },
+) {
+  try {
+    const connection = new Connection(RPC_URL);
+    const referralProvider = new ReferralProvider(connection);
+
+    const body = await request.json();
+
+    const { referralAccount, feePayer } = bodyShape.parse({
+      ...body,
+      referralAccount: address,
+    });
+
+    let feePayerPubkey = new PublicKey(feePayer);
+    const txsCompiled = await referralProvider.claimAll({
+      payerPubKey: feePayerPubkey,
+      referralAccountPubKey: new PublicKey(referralAccount),
+    });
+
+    const txs = txsCompiled.map((tx) =>
+      Buffer.from(tx.serialize()).toString("base64"),
+    );
+
+    return NextResponse.json({
+      txs: txs,
+    });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 400 });
+  }
 }
