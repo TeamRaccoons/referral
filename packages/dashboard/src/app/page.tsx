64e6783dab88cac24878e905c3e7f05be2407771
aman <amanjemal71@gmail.com>
2025-05-27 20:16:53 +0800

    feat: UI updates

--- packages/dashboard/src/app/page.tsx
+++ packages/dashboard/src/app/page.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { DoorOpen, Square, Star, Triangle } from "lucide-react";
 import { Balancer } from "react-wrap-balancer";
 
 import { buttonVariants } from "@/components/ui/button";
@@ -21,18 +22,17 @@ export default function Home() {
           className="animate-fade-up from-foreground to-muted-foreground bg-gradient-to-br bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] opacity-0 drop-shadow-sm dark:text-[#FCFCFD] md:text-6xl/[5rem]"
           style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
         >
-          <Balancer>
-            Your <span className="text-[#2E90FA]">all-in-one</span> wealth
-            accumulation
-          </Balancer>
+          <Balancer>Jupiter Referral Dashboard</Balancer>
         </h1>
         <p
           className="animate-fade-up mt-6 text-center opacity-0 dark:text-[#98A2B3] md:text-xl"
           style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
         >
           <Balancer>
-            Easiest way to create a referral account for your Solana program
+            The Jupiter Referral Program enables developers to earn fees when
+            integrating the Jupiter APIs.
           </Balancer>
+          <Balancer> (Anyone can also use this for their own Program)</Balancer>
         </p>
         <div
           className="animate-fade-up mx-auto mt-6 flex items-center justify-center space-x-5 opacity-0"
@@ -40,46 +40,13 @@ export default function Home() {
         >
           <a
             className={cn(buttonVariants({ variant: "default" }))}
-            href={siteConfig.github}
+            href="https://dev.jup.ag/docs/tool-kits/referral-program"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <GitHub className="mr-1 h-4 w-4" />
-            <span>Star on GitHub</span>
+            <DoorOpen className=" mr-1 h-4 w-4 " />
+            <span className="text-center">Documentation</span>
           </a>
-        </div>
-      </div>
-      <div className="animate-fade-up my-16 w-full max-w-screen-lg gap-5 border-t p-5 xl:px-0">
-        <h2 className="pt-4 text-center text-3xl font-bold dark:text-[#98A2B3] md:text-4xl">
-          The Solana Solution
-        </h2>
-
-        <p className="pb-8 pt-4 text-center text-lg dark:text-[#475467]">
-          <Balancer>
-            Solana programs need a way to incentivize developers to use their
-            contracts. We provide a simple way to create a referral account for
-            your Solana program. Start earning today!
-          </Balancer>
-        </p>
-
-        <div className="grid grid-cols-1 gap-5 pt-4 md:grid-cols-3">
-          {marketingFeatures.map((feature) => (
-            <Card key={feature.title} className={cn("p-2")}>
-              <CardHeader>
-                <span className="w-fit rounded-xl p-4 dark:bg-[#344054]">
-                  {feature.icon}
-                </span>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <CardTitle className="dark:text-[#98A2B3]">
-                  {feature.title}
-                </CardTitle>
-                <CardDescription className="mt-2 text-sm/6 dark:text-[#667085]">
-                  {feature.body}
-                </CardDescription>
-              </CardContent>
-            </Card>
-          ))}
         </div>
       </div>
     </main>
